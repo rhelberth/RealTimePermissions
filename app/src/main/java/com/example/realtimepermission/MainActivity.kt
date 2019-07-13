7ec624169a6128a6e7544ee9738af228e0a63b33
Rhelberth <rhelberth@gmail.com>
2019-07-13 17:41:35 -0300

    first

---
+++ app/src/main/java/com/example/realtimepermission/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.realtimepermission
+
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+    private val permissoes = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //Validar permissões
+        Permissoes.validarPermissoes(permissoes, this, 1)
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, @NonNull permissions: Array<String>, @NonNull grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        for (permissaoResultado in grantResults) {
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED) {
+                alertaValidacaoPermissao()
+            }
+        }
+
+    }
+
+    private fun alertaValidacaoPermissao() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Permissões Negadas")
+        builder.setMessage("Para utilizar o app é necessário aceitar as permissões")
+        builder.setCancelable(false)
+        builder.setPositiveButton("Confirmar") { _, _ ->  finish()}
+
+        val dialog = builder.create()
+        dialog.show()
+
+    }
+
+}
---
+++ app/src/main/java/com/example/realtimepermission/Permissoes.kt
@@ -0,0 +1,43 @@
+package com.example.realtimepermission
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
+import java.util.*
+
+object Permissoes {
+
+    fun validarPermissoes(permissoes: Array<String>, activity: Activity, requestCode: Int): Boolean {
+
+        if (Build.VERSION.SDK_INT >= 23) {
+
+            val listaPermissoes = ArrayList<String>()
+
+            /*Percorre as permissões passadas,
+            verificando uma a uma
+            * se já tem a permissao liberada */
+            for (permissao in permissoes) {
+                val temPermissao = checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED
+
+                if (!temPermissao)
+                    listaPermissoes.add(permissao)
+            }
+            /*Caso a lista esteja vazia, não é necessário solicitar permissão*/
+            if (listaPermissoes.isEmpty())
+                return true
+
+            val novasPermissoes = arrayOfNulls<String>(listaPermissoes.size)
+            listaPermissoes.toArray(novasPermissoes)
+
+            //Solicita permissão
+            ActivityCompat.requestPermissions(activity, novasPermissoes, requestCode)
+            return false
+        }
+
+        return true
+
+    }
+
+}
